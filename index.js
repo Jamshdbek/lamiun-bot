091015b742630e3486dc63ee0f33e6a2dfae6842
Jamshid <jamshid.ibro@gmail.com>
2024-11-15 15:26:36 +0500

    fix: start errors

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const TelegramBot = require("node-telegram-bot-api");
-const { Telegram, Markup } = require("telegraf");
+const { Telegram } = require("telegraf");
 require("dotenv").config();
 const token = process.env.SECRET_TOKEN;
-const apiUrl = process.env.API_BACKEND;
 const bot = new TelegramBot(token, { polling: true });
 const bott = new Telegram(token, { polling: true });
-const userData = {};
-const app_url = "https://ibro.uz";
 
 function requestSignIn(param) {
   console.log(param, "param");
-  const urlPush = `https://lamiun-web-cys3.vercel.app`;
+  let urlPush = `https://lamiun-web-cys3.vercel.app`;
   try {
     const res = fetch("http://localhost:8080/auth/login", {
       method: "POST",
@@ -22,16 +19,11 @@ function requestSignIn(param) {
         password: "234",
       }),
     });
-    console.log(
-      res.then((res) => {
-        console.log(res);
-      }),
-      "res.status"
-    );
+
     if (res.ok) {
-      return (urlPush = "https://lamiun-web-cys3.vercel.app/dashboard");
+      urlPush = "https://lamiun-web-cys3.vercel.app/dashboard";
     } else {
-      return (urlPush = `https://lamiun-web-cys3.vercel.app?search=${param.username}`);
+      urlPush = `https://lamiun-web-cys3.vercel.app?search=${param.username}`;
     }
   } catch (error) {
     console.error("Error:", error);
@@ -40,10 +32,11 @@ function requestSignIn(param) {
   return urlPush;
 }
 
+
 bott.setChatMenuButton({
   menuButton: {
     type: "web_app",
-    text: "Web App",
+    text: "Ochish",
     web_app: {
       url: `https://lamiun-web-cys3.vercel.app`,
     },
@@ -55,30 +48,28 @@ bot.on("message", async (msg) => {
   const text = msg.text;
 
   // req ui backend
-  // requestSignIn(msg.from);
-  // start chat
   if (text === "/start") {
     await bot.sendMessage(
       chatId,
-      "Assalomu Alaykum \n\n *Lamiun* -  Arab tilini boshlang'ich bilimlarni o'rganishga yordam beruvchi platforma.   \n\n",
+      "Assalomu Alaykum \n\n *Lamiun* -  Arab tilini boshlang'ich bilimlarni o'rganishga yordam beruvchi platforma.   \n\n  Ilovadan foydalanish uchun pastdagi *ochish* tugmasin bosing!📱",
       { parse_mode: "Markdown" }
     );
-    await bot.setch;
-    await bot.sendMessage(chatId, " \n\nPlatforma ni ochish 📱", {
-      parse_mode: "Markdown",
-      reply_markup: {
-        inline_keyboard: [
-          [
-            {
-              text: "Lamiun",
-              web_app: {
-                url: requestSignIn(msg.from),
-              },
-            },
-          ],
-        ],
-      },
-    });
+    // await bot.setch;
+    // await bot.sendMessage(chatId, " \n\nPlatforma ni ochish 📱", {
+    //   parse_mode: "Markdown",
+    //   reply_markup: {
+    //     inline_keyboard: [
+    //       [
+    //         {
+    //           text: "Lamiun",
+    //           web_app: {
+    //             url: requestSignIn(msg.from),
+    //           },
+    //         },
+    //       ],
+    //     ],
+    //   },
+    // });
 
     // bot.sendMessage(
     //   chatId,
